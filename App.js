70e0ab4ad3057cbc3d122af4d0ed59c63054fbcd
randomdev1234 <randomdev1234@protonmail.com>
2021-09-09 13:32:33 -0600

    WIP

--- App.js
+++ App.js
@@ -10,18 +10,18 @@ export default function App () {
 
       <FlatList
         data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jillian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
+          { key: 'Devin' },
+          { key: 'Dan' },
+          { key: 'Dominic' },
+          { key: 'Jackson' },
+          { key: 'James' },
+          { key: 'Joel' },
+          { key: 'John' },
+          { key: 'Jillian' },
+          { key: 'Jimmy' },
+          { key: 'Julie' }
         ]}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
       />
     </View>
   )
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const webpack = require('webpack')
+require('webpack')
 
 const appDirectory = path.resolve(__dirname, '../')
 
